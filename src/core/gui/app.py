412ee3a4da15b4dc60385605e90710d02d9042fc
matheus_cerozi <matheus.cerozi@jobconvo.com>
2023-04-12 12:47:00 -0300

    typing IBaseFrame

--- src/core/gui/app.py
+++ src/core/gui/app.py
@@ -3,6 +3,8 @@ import customtkinter
 from src.core.gui.frames.chat import ChatFrame
 from src.core.gui.frames.login import LoginFrame
 
+from src.core.gui.frames.interface.base_frame import IBaseFrame
+
 class ChatGUI(customtkinter.CTk):
 
     WIDTH = 400
@@ -22,12 +24,12 @@ class ChatGUI(customtkinter.CTk):
         self.display_frame(self.login)
         self.login.button.configure(command = lambda: self.switch_frames(self.chat, self.login))
 
-    def display_frame(self, frame: customtkinter.CTkFrame) -> None:
+    def display_frame(self, frame: IBaseFrame) -> None:
         
         frame.init_widgets()
         frame.pack(expand = True)
 
-    def switch_frames(self, new_frame: customtkinter.CTkFrame, old_frame: customtkinter.CTkFrame) -> None:
+    def switch_frames(self, new_frame: IBaseFrame, old_frame: IBaseFrame) -> None:
         
         old_frame.pack_forget()
         self.display_frame(new_frame)
